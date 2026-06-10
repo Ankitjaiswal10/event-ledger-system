cf3059063524735700236c906e4e6423e3e65dba
Ankitjaiswal10 <47365815+Ankitjaiswal10@users.noreply.github.com>
2026-06-10 15:12:05 +0530

    Create AccountRepository interface for account entities and repository package

---
+++ account-service/src/main/java/com/eventledger/account/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.eventledger.account.repository;
+
+import com.eventledger.account.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository
+        extends JpaRepository<AccountEntity,Long> {
+
+    Optional<AccountEntity> findByAccountId(String accountId);
+}
