906e518f910306089b60d242fcc86e9adf4d86f8
Ankitjaiswal10 <47365815+Ankitjaiswal10@users.noreply.github.com>
2026-06-10 15:10:29 +0530

    Add AccountNotFoundException class and exception module

---
+++ account-service/src/main/java/com/eventledger/account/exception/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.eventledger.account.exception;
+
+public class AccountNotFoundException
+        extends RuntimeException {
+
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
